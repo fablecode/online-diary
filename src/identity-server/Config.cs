409acc6206ac4116e5151153049c33a02ee02e77
fablecode <fablecode@users.noreply.github.com>
2024-08-27 20:02:19 +0200

    Identity Server project added.

---
+++ src/identity-server/Config.cs
@@ -0,0 +1,40 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace identity_server;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        { 
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+            new("diaryApi", "Diary API")
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            new()
+            {
+                ClientId = "client",
+
+                // no interactive user, use the clientid/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                // secret for authentication
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+
+                // scopes that client has access to
+                AllowedScopes = { "diaryApi" }
+            }
+        };
+}
